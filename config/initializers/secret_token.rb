2b6ffc79db051590dc6561cb8819eb5ca08fb188
Jakub Sevcech <sevo_jakub@yahoo.fr>
2011-08-30 23:16:47 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowler::Application.config.secret_token = 'bfbd0467c161c43bf72c9054861fa54b7385c9443bed249b4b1ce964a2793738bae885ce0c8bba5f3ddbdbad48d1a11daee7d0d4993acf539d9f787d9b120566'
